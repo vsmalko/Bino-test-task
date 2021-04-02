c7a5f741f67acd272760a3f0603a72161265bf0c
Veronika Smalko <v.smalko16@gmail.com>
2021-04-02 16:03:22 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+//AOS Animations initialization
+AOS.init();
+
 // Slider initialization
 $(document).ready(function () {
   $(".slider").slider();
@@ -14,6 +17,23 @@ $(document).ready(function () {
 $(document).ready(function () {
   $(".fixed-action-btn").floatingActionButton();
 });
+$(".btn-floating").on("click", function() {
+  window.scrollTo(0, 0);
+});
+$(".btn-floating").on("click", function () {
+  window.scrollTo(0, 0);
+});
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    $(".btn-floating").show();
+  } else {
+    $(".btn-floating").hide();
+  }
+}
 
 // Counter animation
 function animateValue(obj, start, end, duration) {
@@ -43,3 +63,40 @@ animateValue(campaigns, 0, 178, 2000);
 
 const features = document.querySelector("#features");
 animateValue(features, 0, 285, 2000);
+
+// Team
+
+$(".team__person_1").on("mouseover", function() {
+  $("#teamtext1").addClass("show");
+});
+$(".team__person_1").on("mouseout", function () {
+  $("#teamtext1").removeClass("show");
+});
+ 
+$(".team__person_2").on("mouseover", function () {
+  $("#teamtext2").addClass("show");
+});
+$(".team__person_2").on("mouseout", function () {
+  $("#teamtext2").removeClass("show");
+});
+
+$(".team__person_3").on("mouseover", function () {
+  $("#teamtext3").addClass("show");
+});
+$(".team__person_3").on("mouseout", function () {
+  $("#teamtext3").removeClass("show");
+});
+
+$(".team__person_4").on("mouseover", function () {
+  $("#teamtext4").addClass("show");
+});
+$(".team__person_4").on("mouseout", function () {
+  $("#teamtext4").removeClass("show");
+});
+
+$(".team__person_5").on("mouseover", function () {
+  $("#teamtext5").addClass("show");
+});
+$(".team__person_5").on("mouseout", function () {
+  $("#teamtext5").removeClass("show");
+});
