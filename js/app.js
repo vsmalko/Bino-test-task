4a42dd0f6393ae5dcced3e1b2d4b60c32e98e2e4
Veronika Smalko <v.smalko16@gmail.com>
2021-04-02 17:38:17 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -13,6 +13,43 @@ $(document).ready(function () {
   $(".carousel").carousel();
 });
 
+
+// Team events
+$(".team__person_1").on("mouseover", function() {
+  $("#teamtext1").addClass("show");
+});
+$(".team__person_1").on("mouseout", function () {
+  $("#teamtext1").removeClass("show");
+});
+ 
+$(".team__person_2").on("mouseover", function () {
+  $("#teamtext2").addClass("show");
+});
+$(".team__person_2").on("mouseout", function () {
+  $("#teamtext2").removeClass("show");
+});
+
+$(".team__person_3").on("mouseover", function () {
+  $("#teamtext3").addClass("show");
+});
+$(".team__person_3").on("mouseout", function () {
+  $("#teamtext3").removeClass("show");
+});
+
+$(".team__person_4").on("mouseover", function () {
+  $("#teamtext4").addClass("show");
+});
+$(".team__person_4").on("mouseout", function () {
+  $("#teamtext4").removeClass("show");
+});
+
+$(".team__person_5").on("mouseover", function () {
+  $("#teamtext5").addClass("show");
+});
+$(".team__person_5").on("mouseout", function () {
+  $("#teamtext5").removeClass("show");
+});
+
 // Floating action button initialization
 $(document).ready(function () {
   $(".fixed-action-btn").floatingActionButton();
@@ -63,40 +100,3 @@ animateValue(campaigns, 0, 178, 2000);
 
 const features = document.querySelector("#features");
 animateValue(features, 0, 285, 2000);
-
-// Team
-
-$(".team__person_1").on("mouseover", function() {
-  $("#teamtext1").addClass("show");
-});
-$(".team__person_1").on("mouseout", function () {
-  $("#teamtext1").removeClass("show");
-});
- 
-$(".team__person_2").on("mouseover", function () {
-  $("#teamtext2").addClass("show");
-});
-$(".team__person_2").on("mouseout", function () {
-  $("#teamtext2").removeClass("show");
-});
-
-$(".team__person_3").on("mouseover", function () {
-  $("#teamtext3").addClass("show");
-});
-$(".team__person_3").on("mouseout", function () {
-  $("#teamtext3").removeClass("show");
-});
-
-$(".team__person_4").on("mouseover", function () {
-  $("#teamtext4").addClass("show");
-});
-$(".team__person_4").on("mouseout", function () {
-  $("#teamtext4").removeClass("show");
-});
-
-$(".team__person_5").on("mouseover", function () {
-  $("#teamtext5").addClass("show");
-});
-$(".team__person_5").on("mouseout", function () {
-  $("#teamtext5").removeClass("show");
-});
